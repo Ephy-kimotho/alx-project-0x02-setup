2743ad163d300ec37bf27ba0ecd57723d6dd344a
Ephy Kimotho <ephyk48@gmail.com>
2025-01-12 15:50:42 +0300

    created components and interfaces directories and a welcome message in pages/index.tsx

---
+++ alx-project-2/pages/index.tsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <section className="bg-zinc-200 min-h-screen grid place-items-center">
+      <h2 className="text-teal-600 text-2xl font-bold">
+        Welcome to my Next JS app.
+      </h2>
+    </section>
+  );
+}
+
+export default Home;
