8cabdeff2998afe0b568a2e5bb0c6ae9c043dc68
Ephy Kimotho <ephyk48@gmail.com>
2025-01-12 20:53:19 +0300

    Implemented a Post page

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,12 +1,33 @@
 import Header from "@/components/layout/Header";
+import PostCard from "@/components/common/PostCard";
 
 function Posts() {
   return (
     <section className="bg-zinc-200 min-h-screen">
       <Header />
-      <h2 className="text-green-600 text-2xl ml-4 font-bold">
-        This is the posts page
-      </h2>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 m-4 gap-4">
+        <PostCard
+          title="sunt aut facere repellat providen."
+          content="quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+          userId={2}
+        />
+        <PostCard
+          title="sunt aut facere repellat providen."
+          content="quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+          userId={12}
+        />
+        <PostCard
+          title="sunt aut facere repellat providen."
+          content="quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+          userId={4}
+        />
+        <PostCard
+          title="sunt aut facere repellat providen."
+          content="quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+          userId={15}
+        />
+      </div>
     </section>
   );
 }
